8ef91cc6128981098ef726d4bbd13183e07659a6
Faulo <FauloLio@gmail.com>
2018-03-23 11:32:22 +0100

    assets.xml => assets\.xml

--- src/registerModule.php
+++ src/registerModule.php
@@ -1,14 +1,8 @@
 <?php
-
 use Slothsoft\Farah\Module\Module;
 use Slothsoft\Farah\Module\ModuleRepository;
 use Slothsoft\Farah\Module\FarahUrl\FarahUrlAuthority;
-use Slothsoft\Farah\Module\Manifest\XmlManifest;
 
 ModuleRepository::getInstance()->registerModule(
-    new Module(
-        FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'webrtc'),
-        new XmlManifest(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets.xml'),
-        dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets'
-    )
-);
+    new Module(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'webrtc'),
+        dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets'));
