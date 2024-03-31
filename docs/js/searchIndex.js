38887ce5f185b09dbd121f2eaa85349c8487ef1b
Daniel Schulz <faulolio@gmail.com>
2024-04-01 01:26:06 +0200

    build: update files

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Slothsoft\\WebRTC\\Assets\\VideoChatThingBuilder",
+            "name": "VideoChatThingBuilder",
+            "summary": "",
+            "url": "classes/Slothsoft-WebRTC-Assets-VideoChatThingBuilder.html"
+        },                {
+            "fqsen": "\\Slothsoft\\WebRTC\\Assets\\VideoChatThingBuilder\u003A\u003AbuildExecutableStrategies\u0028\u0029",
+            "name": "buildExecutableStrategies",
+            "summary": "",
+            "url": "classes/Slothsoft-WebRTC-Assets-VideoChatThingBuilder.html#method_buildExecutableStrategies"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Slothsoft\\WebRTC\\Assets",
+            "name": "Assets",
+            "summary": "",
+            "url": "namespaces/slothsoft-webrtc-assets.html"
+        },                {
+            "fqsen": "\\Slothsoft\\WebRTC",
+            "name": "WebRTC",
+            "summary": "",
+            "url": "namespaces/slothsoft-webrtc.html"
+        },                {
+            "fqsen": "\\Slothsoft",
+            "name": "Slothsoft",
+            "summary": "",
+            "url": "namespaces/slothsoft.html"
+        }            ]
+);
